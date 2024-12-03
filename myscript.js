5c199fd66e324cfc6dc036475b97f03ac78c94f9
James Wu <jameswlb01@gmail.com>
2024-12-03 16:02:16 +0800

    Add game controller

--- myscript.js
+++ myscript.js
@@ -1,4 +1,4 @@
-// Encapsulate the game 
+// Gameboard IIFE
 const Gameboard = (() => {
     // Initialize the gameboard
     const board = Array(9).fill(null);
@@ -21,3 +21,28 @@ const Gameboard = (() => {
     return { placeMark, resetBoard, getBoard };
 })();
 
+
+// Player Objects
+const Player = (name, mark) => {
+    return { name, mark };
+};
+
+// Game Controller IIFE
+const GameController = (() => {
+    let currentPlayer;
+    const player1 = Player("Player 1", "X");
+    const player2 = Player("Player 2", "O");
+
+    // Reset the game 
+    const startGame = () => {
+        currentPlayer = player1;
+        Gameboard.resetBoard();
+    }
+
+    const handleClick = (index) => {
+        Gameboard.placeMark(index, currentPlayer.mark);
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
+    };
+
+    return { startGame, handleClick };
+})();
